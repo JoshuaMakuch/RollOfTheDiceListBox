547e66eec451bebd4f7d5d2d850cb7568d811060
JoshuaMakuch <joshuamakuch9@gmail.com>
2023-03-09 08:42:31 -0700

    Got the dice roll and basic setup done including tooltips and formatting

--- RollOfTheDiceListBox/RollOfTheDiceForm.Designer.vb
+++ RollOfTheDiceListBox/RollOfTheDiceForm.Designer.vb
@@ -22,10 +22,72 @@ Partial Class RollOfTheDiceForm
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.components = New System.ComponentModel.Container()
+        Me.DisplayListBox = New System.Windows.Forms.ListBox()
+        Me.ExitButton = New System.Windows.Forms.Button()
+        Me.RollButton = New System.Windows.Forms.Button()
+        Me.ClearButton = New System.Windows.Forms.Button()
+        Me.ToolTip1 = New System.Windows.Forms.ToolTip(Me.components)
+        Me.SuspendLayout()
+        '
+        'DisplayListBox
+        '
+        Me.DisplayListBox.Font = New System.Drawing.Font("Consolas", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.DisplayListBox.FormattingEnabled = True
+        Me.DisplayListBox.ItemHeight = 18
+        Me.DisplayListBox.Location = New System.Drawing.Point(12, 10)
+        Me.DisplayListBox.Name = "DisplayListBox"
+        Me.DisplayListBox.Size = New System.Drawing.Size(776, 310)
+        Me.DisplayListBox.TabIndex = 0
+        '
+        'ExitButton
+        '
+        Me.ExitButton.Location = New System.Drawing.Point(536, 347)
+        Me.ExitButton.Name = "ExitButton"
+        Me.ExitButton.Size = New System.Drawing.Size(173, 91)
+        Me.ExitButton.TabIndex = 2
+        Me.ExitButton.Text = "Exit"
+        Me.ToolTip1.SetToolTip(Me.ExitButton, "Exits the form")
+        Me.ExitButton.UseVisualStyleBackColor = True
+        '
+        'RollButton
+        '
+        Me.RollButton.Location = New System.Drawing.Point(76, 347)
+        Me.RollButton.Name = "RollButton"
+        Me.RollButton.Size = New System.Drawing.Size(173, 91)
+        Me.RollButton.TabIndex = 0
+        Me.RollButton.Text = "Roll"
+        Me.ToolTip1.SetToolTip(Me.RollButton, "Rolls a new set of dice")
+        Me.RollButton.UseVisualStyleBackColor = True
+        '
+        'ClearButton
+        '
+        Me.ClearButton.Location = New System.Drawing.Point(309, 347)
+        Me.ClearButton.Name = "ClearButton"
+        Me.ClearButton.Size = New System.Drawing.Size(173, 91)
+        Me.ClearButton.TabIndex = 1
+        Me.ClearButton.Text = "Clear"
+        Me.ToolTip1.SetToolTip(Me.ClearButton, "Clears the current list box")
+        Me.ClearButton.UseVisualStyleBackColor = True
+        '
+        'RollOfTheDiceForm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 20.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Text = "RollOfTheDice"
+        Me.Controls.Add(Me.ClearButton)
+        Me.Controls.Add(Me.RollButton)
+        Me.Controls.Add(Me.ExitButton)
+        Me.Controls.Add(Me.DisplayListBox)
+        Me.Name = "RollOfTheDiceForm"
+        Me.Text = "Roll Of The Dice"
+        Me.ResumeLayout(False)
+
     End Sub
 
+    Friend WithEvents DisplayListBox As ListBox
+    Friend WithEvents ExitButton As Button
+    Friend WithEvents RollButton As Button
+    Friend WithEvents ClearButton As Button
+    Friend WithEvents ToolTip1 As ToolTip
 End Class
--- RollOfTheDiceListBox/RollOfTheDiceForm.vb
+++ RollOfTheDiceListBox/RollOfTheDiceForm.vb
@@ -2,11 +2,40 @@
 'RCET0265
 'Spring 2023
 'Rolld Of The Dice List Box
-'
+'https://github.com/JoshuaMakuch/RollOfTheDiceListBox.git
 
 Option Strict On
 Option Explicit On
 
 Public Class RollOfTheDiceForm
+
+    'Exits the form when exit button clicked
+    Private Sub ExitButton_Click(sender As Object, e As EventArgs) Handles ExitButton.Click
+        Me.Close()
+    End Sub
+
+    'Clears the list box when the clear button is clicked
+    Private Sub ClearButton_Click(sender As Object, e As EventArgs) Handles ClearButton.Click
+
+    End Sub
+
+    'Activates the roll the dice code (from the previous code) when button is pressed
+    Private Sub RollButton_Click(sender As Object, e As EventArgs) Handles RollButton.Click
+        Dim rolls(10) As Integer 'Initializes an array conatining 11 toltal values
+        Dim roll1 As Integer
+        Dim roll2 As Integer
+        Dim Total As Integer
+        Dim random As New Random() 'Creates an instance of the random class and the new generates a new seed each run through to provide pseudo randomness
+
+        For i As Integer = 0 To 999
+            roll1 = random.Next(1, 7) 'First die roll. Also the .next generates a random integer (>= first and < second), but .NextDouble can be used to generate a random double
+            roll2 = random.Next(1, 7) 'Second die roll
+            Total = roll1 + roll2 'The combination of the two rolls
+            rolls(Total - 2) += 1 'Increments the amount of times the total was rolled based on what the total is. (Shifted -2 to match appropriate array value)
+        Next
+
+        DisplayListBox.Text() = "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA"
+
+    End Sub
 
 End Class
